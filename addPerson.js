7f3661bc50c1fbab9180933f36e211c17948bfae
Erin Zimmer <ejzimmer@gmail.com>
2022-07-09 09:26:29 +1000

    first cut of addPerson netlify function

---
+++ addPerson.js
@@ -0,0 +1,19 @@
+// Grab our credentials from a .env file or environment variables
+require("dotenv").config()
+
+const { DATABASE_URL, SUPABASE_SERVICE_API_KEY } = process.env
+
+// Connect to our database
+const { createClient } = require("@supabase/supabase-js")
+const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
+
+// Our standard serverless handler function
+exports.handler = async (event) => {
+  const person = JSON.parse(event.body).person
+
+  // Insert a row
+  const { data, error } = await supabase.from("sizes").insert([person])
+
+  // Did it work?
+  console.log(data, error)
+}
