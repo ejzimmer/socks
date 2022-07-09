f959d5e016252524aad70dd7a587412af08d3870
Erin Zimmer <ejzimmer@gmail.com>
2022-07-10 09:56:30 +1000

    better logging

--- netlify/functions/listPeople/listPeople.js
+++ netlify/functions/listPeople/listPeople.js
@@ -3,6 +3,9 @@ const { supabase } = require("../../../getSupabase.js")
 const handler = async () => {
   const { data, error } = await supabase.from("sizes").select("*")
 
+  console.log(data)
+  console.log(error)
+
   return {
     statusCode: 200,
     data,
