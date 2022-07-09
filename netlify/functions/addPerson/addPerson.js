d02414d649f4001b9ae57134df3849e39d91e26e
Erin Zimmer <ejzimmer@gmail.com>
2022-07-09 11:07:44 +1000

    split up args

--- netlify/functions/addPerson/addPerson.js
+++ netlify/functions/addPerson/addPerson.js
@@ -9,12 +9,13 @@ const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
 
 // Our standard serverless handler function
 exports.handler = async (event) => {
-  const person = event.body
-  console.log(person)
+  const person = Object.fromEntries(
+    event.body.split("&").map((entry) => entry.split("="))
+  )
 
   // Insert a row
-  // const { data, error } = await supabase.from("sizes").insert([person])
+  const { data, error } = await supabase.from("sizes").insert([person])
 
   // Did it work?
-  // console.log(data, error)
+  console.log(data, error)
 }
