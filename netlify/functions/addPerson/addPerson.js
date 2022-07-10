293689c598dc7ac97efb8152e0fcdb2789ff7cea
Erin Zimmer <ejzimmer@gmail.com>
2022-07-10 11:20:42 +1000

    better info

--- netlify/functions/addPerson/addPerson.js
+++ netlify/functions/addPerson/addPerson.js
@@ -1,11 +1,59 @@
 const { supabase } = require("../../../getSupabase.js")
 
+const SIZES = {
+  38: {
+    circumference: 8,
+    length: 237,
+  },
+  39: {
+    circumference: 8,
+    length: 246,
+  },
+  40: {
+    circumference: 8,
+    length: 254,
+  },
+  41: {
+    circumference: 9,
+    length: 258,
+  },
+  42: {
+    circumference: 9,
+    length: 262,
+  },
+  43: {
+    circumference: 9,
+    length: 271,
+  },
+  44: {
+    circumference: 10,
+    length: 275,
+  },
+  45: {
+    circumference: 10,
+    length: 283,
+  },
+  46: {
+    circumference: 10,
+    length: 288,
+  },
+}
+
 exports.handler = async (event) => {
   const person = Object.fromEntries(
     event.body.split("&").map((entry) => entry.split("="))
   )
 
-  const { data, error } = await supabase.from("sizes").insert([person])
+  const circumference = person.circumference || SIZES[person.size].circumference
+  const length = person.length || SIZES[person.size].length
+
+  const entry = {
+    name: person.name,
+    circumference,
+    length,
+  }
+
+  const { data, error } = await supabase.from("sizes").insert([entry])
   console.log(data, error)
 
   if (error) {
--- netlify/functions/listPeople/listPeople.js
+++ netlify/functions/listPeople/listPeople.js
@@ -1,7 +1,7 @@
 const { supabase } = require("../../../getSupabase.js")
 
 const handler = async () => {
-  const { data, error } = await supabase.from("sizes").select("*")
+  const { data } = await supabase.from("sizes").select("*")
 
   return {
     statusCode: 200,
